5d8b484f1e79e15d9aed2abb03a852e9a612b97e
Manuel Diago Garcia <manuel@manuelyelena.com>
2025-09-18 05:57:00 +0200

    Trabajos iniciales versión 1.0.0.1
    Trabajos iniciales versión 1.0.0.1

--- NetFramework480/Properties/AssemblyInfo.cs
+++ NetFramework480/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Irene Solutions SL")]
 [assembly: AssemblyProduct("FACe")]
 [assembly: AssemblyCopyright("Copyright © Irene Solutions SL 2025")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Irene Solutions")]
 [assembly: AssemblyCulture("")]
 
 // Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Número de compilación
 //      Revisión
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
